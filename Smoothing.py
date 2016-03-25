ddd75b0ad95b95b58774520173f81f44f0f8b6e7
Gregg Varona <greggvarona@gmail.com>
2016-03-25 21:09:05 +0800

    Fix incorrect sorting of the pixels for median filter.

--- Smoothing.py
+++ Smoothing.py
@@ -3,12 +3,12 @@ from operator import itemgetter
 import ImageUtils as util
 from datetime import datetime
 
-def median(input, w, h, window, ww, wh):
+def median(input, w, h, ww, wh):
     output = []
+    window = [(0, 0, 0) for el in range(ww * wh)]
     for row in range(h):
         for col in range(w):
             #origin = row * w + col
-            window = [(0,0,0), (0,0,0), (0,0,0)] * 3
             for windowY in range(wh):
                 for windowX in range(ww):
                     y = row + windowY - (wh/2)
@@ -20,25 +20,18 @@ def median(input, w, h, window, ww, wh):
                             index = y * w + x
                             wIndex = windowY * ww + windowX
                             window[wIndex] = input[index]
-            #print "window: " + str(row) + " " + str(col) + str(window[len(sorted(window, key=itemgetter(0, 1, 2))) / 2])
-            #print sorted(window, key=itemgetter(0, 1, 2))
-            output.append(window[len(sorted(window, key=itemgetter(0, 1, 2))) / 2])
-            #output.append(tuple([clampRGBValue(r), clampRGBValue(g), clampRGBValue(b), clampRGBValue(a)]))
+            window = sorted(window, key=itemgetter(0, 1, 2))
+            output.append(window[len(window) / 2])
     return output
 
 def main():
-    im = util.open('in/very-small.png')
+    im = util.open('in/captcha.png')
     arr = util.toBytes(im)
-    window = [(0,0,0), (0,0,0), (0,0,0)] * 3
     out = []
     fileMedian = "out/m-" + datetime.now().strftime('%Y%m%d%H%M%S') + ".png"
     print("Executing... ")
-    print("input ")
-    #print arr
-    out = median(arr, im.width, im.height, window, 3, 3)
+    out = median(arr, im.width, im.height, 3, 3)
     util.save(fileMedian, out, im.mode, im.size)
-    print("out ")
-    #print out
     print("done saving ")
 
 if __name__ == "__main__":
