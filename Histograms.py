b7af1702d9bbeb5ded111305b3c24f1648009e2b
Gregg Varona <greggvarona@gmail.com>
2016-04-04 15:35:53 +0800

    Added Histogram.equalize.

--- Histograms.py
+++ Histograms.py
@@ -2,6 +2,7 @@ import ImageUtils as util
 import GrayScale as grayScale
 from datetime import datetime
 import math
+from decimal import *
 
 def frequencyDistribution(input, w, h, colorIndex):
     output = [0] * 256
@@ -43,16 +44,38 @@ def brightest(histogram):
     maximum = ctr
     return maximum
 
+def equalize(input, w, h, colorIndex):
+    theRange = 255
+    area = w * h
+    factor = Decimal(theRange) / Decimal(area)
+    histogram = frequencyDistribution(input, w, h, colorIndex)
+    output = []
+    sum = 0
+    for level in range(256):
+        sum = sum + histogram[level]
+        histogram[level] = int(math.floor(factor * sum + Decimal(0.00001)))
+
+    for row in range(h):
+        for col in range(w):
+            index = row * w + col
+            rgb = list(input[index])
+            newRgb = histogram[rgb[colorIndex]]
+            output.append(tuple([newRgb, newRgb, newRgb]))
+    return output
+
 def main():
     print 'Executing Histograms.main'
-    im = util.open('in/DSC_0132.JPG')
+    im = util.open('in/captcha.jpg')
     fileName = "out/normalized-" + datetime.now().strftime('%Y%m%d%H%M%S') + ".jpg"
+    fileName2 = "out/equalized-" + datetime.now().strftime('%Y%m%d%H%M%S') + ".jpg"
     arr = util.toBytes(im)
     arr = grayScale.averaging(arr)
     util.save("out/tempGray.png", arr, im.mode, im.size)
     # doesn't matter which color index bec. it's gray.
-    arr = normalize(arr, im.width, im.height, 0)
-    util.save(fileName, arr, im.mode, im.size)
+    normalized = normalize(arr, im.width, im.height, 0)
+    equalized = equalize(arr, im.width, im.height, 0)
+    util.save(fileName, normalized, im.mode, im.size)
+    util.save(fileName2, equalized, im.mode, im.size)
     print 'Done executing Histograms.main'
 
 if __name__ == "__main__":
